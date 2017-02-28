ffca45dc0fb3504d90046ee5e3369ecc15e115a9
Kevin <kevingm31@gmail.com>
2017-02-27 20:49:40 -0600

    Finished Code

--- store_item.rb
+++ store_item.rb
@@ -1,20 +1,20 @@
-bat1 = {"name" => "Gregory", "color" => "black", "type" => "friendly"}
-bat2 = {"name" => "Todd", "color" => "blackish", "type" => "stoic"}
-bat3 = {"name" => "Lacy", "color" => "brown", "type" => "noisy"}
+# bat1 = {"name" => "Gregory", "color" => "black", "type" => "friendly"}
+# bat2 = {"name" => "Todd", "color" => "blackish", "type" => "stoic"}
+# bat3 = {"name" => "Lacy", "color" => "brown", "type" => "noisy"}
 
-bat1 = {:name => "Gregory", :color => "black", :type => "friendly"}
-bat2 = {:name => "Todd", :color => "blackish", :type => "stoic"}
-bat3 = {:name => "Lacy", :color => "brown", :type => "noisy"}
+# bat1 = {:name => "Gregory", :color => "black", :type => "friendly"}
+# bat2 = {:name => "Todd", :color => "blackish", :type => "stoic"}
+# bat3 = {:name => "Lacy", :color => "brown", :type => "noisy"}
 
-puts "#{bat1["name"]} is a #{bat1["color"]} bat who is super #{bat1["type"]}"
+# puts "#{bat1["name"]} is a #{bat1["color"]} bat who is super #{bat1["type"]}"
 
-bat1 = {name: "Gregory", color: "black", type: "friendly"}
-bat2 = {name: "Todd", color: "blackish", type: "stoic"}
-bat3 = {name: "Lacy", color: "brown", type: "noisy"}
+# bat1 = {name: "Gregory", color: "black", type: "friendly"}
+# bat2 = {name: "Todd", color: "blackish", type: "stoic"}
+# bat3 = {name: "Lacy", color: "brown", type: "noisy"}
 
-puts "#{bat1[:name]} is a #{bat1[:color]} bat who is super #{bat1[:type]}"
-puts "#{bat2[:name]} is a #{bat2[:color]} bat is often too #{bat2[:type]}"
-puts "#{bat3[:name]} is a #{bat3[:color]} bat is just too #{bat3[:type]}"
+# puts "#{bat1[:name]} is a #{bat1[:color]} bat who is super #{bat1[:type]}"
+# puts "#{bat2[:name]} is a #{bat2[:color]} bat is often too #{bat2[:type]}"
+# puts "#{bat3[:name]} is a #{bat3[:color]} bat is just too #{bat3[:type]}"
 
  
 class Bat
@@ -47,7 +47,9 @@ bat1 = Bat.new("Gregory", "black", "friendly")
 bat2 = Bat.new("Todd", "blackish", "stoic")
 bat2 = Bat.new("Lacy", "brown", "noisy")
 
-puts bat1.name
+#puts bat1.name
 puts bat1.color
 puts bat1.type
+p bat1.change_name("Henry")
+
 
